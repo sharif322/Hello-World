648f2d3968876bebc32c9ffddec4b4aa3b3ba4f2
sharif322 <sharifboy.barotov@gmail.com>
2022-08-22 11:08:24 +0500

    My First Commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.helloworld
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("ShowToast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+         button.setOnClickListener {
+             Log.v("hello world", "button")
+             Toast.makeText(this, "hi sharifboy how are you ", Toast.LENGTH_SHORT).show()
+         }
+    }
+
+    }
